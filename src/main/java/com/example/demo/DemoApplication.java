bdc381848e15ec9dc26cc7dfcd0acb519e101d00
admin <wyjnug411@gmail.com>
2025-09-10 10:39:24 +0900

    2주차

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo; 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	// 아래부터 시작점
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
