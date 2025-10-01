cfc11111aa70b49dd5c26eaafc6dbefb7fbbdd03
admin <wyjnug411@gmail.com>
2025-10-01 11:05:43 +0900

    5주차 게시판생성

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,4 +1,6 @@
-package com.example.demo
+package com.example.demo.controller
+;
+import org.springframework.beans.factory.annotation.Autowired
 ;
 import org.springframework.stereotype.Controller
 ;
@@ -6,8 +8,15 @@ import org.springframework.ui.Model
 ;
 import org.springframework.web.bind.annotation.GetMapping
 ;
+import com.example.demo.model.domain.TestDB
+;
+import com.example.demo.model.service.TestService
+; // 최상단 서비스 클래스 연동 추가
+
 @Controller // 컨트롤러 어노테이션 명시
 public class DemoController{
+  @Autowired
+  TestService testService; // DemoController 클래스 아래 객체 생성
   @GetMapping("/hello") // 전송 방식 GET
   public String hello(Model model) {
     model.addAttribute("data", " 방갑습니다."); // model 설정
@@ -29,4 +38,18 @@ public class DemoController{
     model.addAttribute("para2", 002);
     return "thymeleaf_test1";
   }
+
+  // 하단에 맵핑 이어서 추가
+  @GetMapping("/testdb")
+    public String getAllTestDBs(Model model) {
+      TestDB test = testService.findByName("홍길동");
+      model.addAttribute("data4", test);
+      System.out.println("데이터 출력 디버그 : " + test);
+      return "testdb";
+  }
+
+  // @GetMapping("/article_list")
+  //   public String article_list() {
+  //     return "article_list";
+  //   }
 }
