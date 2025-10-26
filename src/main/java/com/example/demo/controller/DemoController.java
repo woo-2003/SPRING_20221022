6ca53c36e675271431381ec24ac08857aaeb225b
admin <wyjnug411@gmail.com>
2025-10-26 17:49:05 +0900

    2주차 연습문제

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -23,6 +23,16 @@ public class DemoController{
     return "hello"; // hello.html 연결
   }
 
+   @GetMapping("/hello2") // 전송 방식 GET
+  public String hello2(Model model) {
+    model.addAttribute("data1", " 홍길동님."); // model 설정
+    model.addAttribute("data2", " 방갑습니다.");
+    model.addAttribute("data3", " 오늘.");
+    model.addAttribute("data4", " 날씨는.");
+    model.addAttribute("data5", " 매우 좋습니다.");
+    return "hello2"; // hello.html 연결
+  }
+  
   @GetMapping("/about_detailed")
   public String about() {
   return "about_detailed";
